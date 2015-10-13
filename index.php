fbdf490d0d64ad4646c5e81d3754541adb54df71
Paco Bahamonde <paco@juggernautsystems.com.au>
2015-10-14 10:49:25 +1100

    STABLE - Record/Play Back working - Before IMG over IMG

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * Game Home file.
  */
-$app_title = "T W O &nbsp;TO &nbsp;T A N G O";
+$app_title = "T H E &nbsp;R E S T L E S S &nbsp;D A Y S";
 include("include/header_basic.php");
 ?>
 <br /><br />
@@ -11,20 +11,13 @@ include("include/header_basic.php");
 ./images/chars/phil/front-standing-mouth-closed.png
 ./images/chars/ledge/front-standing-mouth-closed.png
 -->
-<canvas width="500" height="220" id="canvas_set"></canvas>
 
-<div style="background: #EAEAEA; width: 50%">
-<b>Aud:</b> TANGO! TANGO! TANGO!<br /><br />
-<b>host:</b> Thank you. Thanks. Your too kind.<br /><br />
-<b>Aud:</b> TANGO! TANGO!<br /><br />
-<b>host:</b> Ok ok. Welcome to Two to Tango. I'm your host 'TANGO'.<br />
-And what do we say here on this show?<br /><br />
-<b>Aud:</b> It takes TWO TO TANGO!!! Yeeaaaaaaahhhhhh.<br /><br />
-<b>host:</b> Let's get started. Today's guest is a man that has been fired from every job he's ever been in. And I'm talking within the first week!<br /><br />
-<b>Aud:</b> Oooohhhhhhhhhh<br /><br />
-<b>host:</b> Please make him Welcome... put your hands together for Bob!<br /><br />
-<b>Aud:</b> Yeeeeeaaaaahhhhhhh.<br />
-</div>
+  <canvas width="500" height="500" id="canvas_set"></canvas>
+  <br />
+  <div id="controls">
+    <input id="record" type="button" value="RECORD" />
+    <input id="play_back" type="button" value="PLAY" />
+  </div>
 <?
 include("include/footer_basic.php");
 ?>
--- js/core.js
+++ js/core.js
@@ -17,21 +17,30 @@ var keys_pressed = [];
 var i = 0;
 var global_time = 0;
 var phil;
+var mimi;
 
 //var image_state = 'left-facing-standing-mouth-closed';
 
 /* OBJECT CONSTRUCTION */
-function character(name, x, y, w, h) {
+function character(name, x, y, w, h, image_state) {
   this.x = x;
   this.y = y;
   this.w = w;
   this.h = h;
   this.name = name;
   this.img = '';
-  this.image_state = 'front-standing-mouth-closed';
+  this.image_state = image_state;
  
   this.img = new Image();
   this.img.src = dir_chars + this.name + '/' + this.image_state + '.png';
+}
+
+function simulateKeyPress(char) {
+  e = jQuery.Event("keydown");
+  e.which = char;
+  e.keyCode = char;
+  // Canvas Object
+  $("#canvas_set").trigger(e);
 }
 
 character.prototype.drawImage = function() {
@@ -48,95 +57,84 @@ character.prototype.charCommands = function(ctx, config) {
   });
   eval(str);
 }
-character.prototype.move = function(ctx, direction, delay){
-
-  global_time += delay;  
-  stationary = 0;
+character.prototype.move = function(ctx, direction){
   
-  var char = this;
+  var stationary = 0;  
+  ctx.clearRect(this.x,this.y,this.w,this.h);
   
-  var callMethod = function () {
-    ctx.clearRect(char.x,char.y,char.w,char.h);
-
-	if(['left','right'].indexOf(direction) != -1) { 
+  if(['left','right'].indexOf(direction) != -1) { 
 			    
-	  if(direction == 'left') {		  
-	    if(char.image_state.indexOf('right') == -1) {
-		  char.image_state = 'right-standing-mouth-closed';
-		  stationary = 1;
-		  
-		} else {
-			
-		  if(char.image_state.indexOf('right-walking-left-foot') == -1) {
-		    char.image_state = 'right-walking-left-foot';
-		  } else {
-		    char.image_state = 'right-walking-right-foot';
-		  }
-		}
-		op = "-";
-	  }
-	  
-	  if(direction == 'right') {
-	    if(char.image_state.indexOf('left') == -1) {
-		  char.image_state = 'left-standing-mouth-closed';
-		  stationary = 1;
-		} else {
-		  if(char.image_state.indexOf('left-walking-left-foot') == -1) {
-		    char.image_state = 'left-walking-left-foot';
-		  } else {
-			char.image_state = 'left-walking-right-foot';
-		  }
-		}
-		op = "+";
-	  }
-	   
+    if(direction == 'left') {		  
+	    if(this.image_state.indexOf('right') == -1) {
+	      this.image_state = 'right-standing-mouth-closed';
+	      stationary = 1;
+	    } else {
+	      if(this.image_state.indexOf('right-walking-left-foot') == -1) {
+	        this.image_state = 'right-walking-left-foot';
+	      } else {
+	        this.image_state = 'right-walking-right-foot';
+	      }
+	    }
+	    op = "-";
+	  }	  
+    if(direction == 'right') {
+      if(this.image_state.indexOf('left') == -1) {
+        this.image_state = 'left-standing-mouth-closed';
+        stationary = 1;
+      } else {
+        if(this.image_state.indexOf('left-walking-left-foot') == -1) {
+          this.image_state = 'left-walking-left-foot';
+        } else {
+          this.image_state = 'left-walking-right-foot';
+        }
+      }
+      op = "+";
+    }
+	  	   
+    if (!stationary) {
+      eval("this.x = this.x " + op + " 10");
+    }
+	
+  } else if(['up','down'].indexOf(direction) != -1) {
+    	  
+    op = "+";
+    if(direction == 'up') {
+      if(this.image_state.indexOf('back') == -1) {
+        this.image_state = 'back-standing';
+        stationary = 1;
+      } else {
+        if(this.image_state.indexOf('back-walking-left-foot') == -1) {
+          this.image_state = 'back-walking-left-foot';
+        } else {
+          this.image_state = 'back-walking-right-foot';
+        }
+      }
+      op = "-";
+    }
+    if(direction == 'down') { 
+      if(this.image_state.indexOf('front') == -1) {
+        this.image_state = 'front-standing-mouth-closed';
+        stationary = 1;
+      } else {
+        if(this.image_state.indexOf('front-walking-right-foot') == -1) {
+          this.image_state = 'front-walking-right-foot';
+        } else {
+          this.image_state = 'front-walking-left-foot';
+        }
+      }
+      op = "+";
+    }
+    
 	  if (!stationary) {
-	    eval("char.x = char.x " + op + " 10");
-	  }
-	} else if(['up','down'].indexOf(direction) != -1) {
-	  
-	  op = "+";
-	  if(direction == 'up') {
-	    if(char.image_state.indexOf('back') == -1) {
-		  char.image_state = 'back-standing';
-		  stationary = 1;
-		} else {
-		  if(char.image_state.indexOf('back-walking-left-foot') == -1) {
-		    char.image_state = 'back-walking-left-foot';
-		  } else {
-			char.image_state = 'back-walking-right-foot';
-		  }
-		}
-		op = "-";
-	  }
-	  if(direction == 'down') {
-	    if(char.image_state.indexOf('front') == -1) {
-		  char.image_state = 'front-standing-mouth-closed';
-		  stationary = 1;
-		} else {
-		  if(char.image_state.indexOf('front-walking-right-foot') == -1) {
-		    char.image_state = 'front-walking-right-foot';
-		  } else {
-			char.image_state = 'front-walking-left-foot';
-		  }
-		}
-		op = "+";
-	  }
-	  if (!stationary) {
-	    eval("char.y = char.y " + op + " 10");
+	    eval("this.y = this.y " + op + " 10");
 	  }
 	}
-
-	char.draw(ctx);
-	
-  };
-  
-  setTimeout(callMethod, global_time);
+ 
+	this.draw(ctx);
   
 }
 
 character.prototype.talk = function(ctx) {
-
   ctx.clearRect(this.x,this.y,this.w,this.h);
 	
   pos1 = 'front';
@@ -146,15 +144,26 @@ character.prototype.talk = function(ctx) {
      this.image_state.indexOf('left') != -1  ||
 	 this.image_state.indexOf('right') != -1) {	
     pos1 = this.image_state.split('-')[0];
+    pos2 = this.image_state.split('-')[1];    
+    if(pos2 != 'sitting') {
+      pos2 = 'standing';
+    }
   }
-    
+      
   if(this.image_state.indexOf('-mouth-closed') == -1) {
     this.image_state = pos1 + '-' + pos2 + '-mouth-closed';  
   } else {
     this.image_state = pos1 + '-' + pos2 + '-mouth-open';	  
   }
-      
+  
+  // TEST IMG-OVER-IMG
+  ctx.globalCompositeOperation = 'destination-over';
+  ctx.clearRect(mimi.x,mimi.y,mimi.w,mimi.h);
+  mimi.drawImage();
   this.draw(ctx);
+  ctx.globalCompositeOperation = 'source-over';
+  
+  // this.draw(ctx);
   
 }
 character.prototype.sit = function(ctx) {
@@ -186,17 +195,47 @@ character.prototype.changeImgSrc = function() {
   //}, global_time);
 }
 
+function createChars() {
+  //Calling this first before drawing characters solves the 
+  //"[refresh] dissapearing canvas" problem.
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+  //TEST IMG-OVER-IMG
+  ctx.globalCompositeOperation = 'destination-over';
+  
+  mimi = new character('Mimi', 75, 400, 90, 120, 'front-standing-mouth-closed');
+  mimi.drawImage();
+ 
+  phil = new character('Phil', 75, 320, 90, 120, 'left-sitting-mouth-closed');
+  phil.drawImage();
+}
+
 $(document).ready(function() {
-	
+  	
+  sound_file = './sounds/episode_01';
+    var mySound = new buzz.sound( sound_file, {
+    formats: [ "mp3" ]
+  });
+  // Play sound file in a loop
+  //mySound.play();
+  
   /*
-  //sound_file = './sounds/careless_whisper';
-  sound_file = './sounds/char_cliff_test_vocals_02';
-  var mySound = new buzz.sound( sound_file, {
+  sound_file = './sounds/ep1-cliff-d01';
+  var mySound1 = new buzz.sound( sound_file, {
     formats: [ "mp3" ]
   });
   // Play sound file in a loop
   // .loop();
-  mySound.play();
+  
+  setTimeout(function(){ mySound1.play(); }, global_time);
+  
+  sound_file = './sounds/ep1-mimi-d01';
+  var mySound2 = new buzz.sound( sound_file, {
+    formats: [ "mp3" ]
+  });
+  global_time += 10000;
+  setTimeout(function() { mySound2.play(); }, global_time);
+  */
   
   // Useful Vars
   // var str_loc = new String(window.location.href);
@@ -209,74 +248,240 @@ $(document).ready(function() {
     sound_file = './sounds/bg';
   }
   */
+    
   canvas = document.getElementById('canvas_set');
   ctx = canvas.getContext("2d");
   
-  phil = new character('Phil', 75, 55, 35, 50);
-  phil.drawImage();
+  //$('#canvas_set').css('background-color', 'green');
+  sets = [];
+  sets[0] = './images/sets/hospital-private-room.png';
+  $('#canvas_set').css('background-image', 'url("' + sets[0] + '")');
   
-  mimi = new character('Mimi', 150, 55, 35, 50);
-  mimi.drawImage();
- 
+  createChars();
+  
+  $('#record').click(function(){    
+    if($(this).val() == 'RECORD') {
+      keys_pressed = [];
+      i = 0;
+      createChars();
+      alert('Move Characters and Key Strokes will be recorded.');
+      mySound.load();
+      mySound.play();
+      $(this).val('STOP RECORDING');
+    } else {
+      mySound.pause();
+      $(this).val('RECORD');
+      $('#play_back').css('display','inline-block');
+      $('#play_back').val('PLAY');
+    }
+  });
+  
+  $('#play_back').css('display','none');
+  
+  $('#play_back').click(function(){
+    
+    if($(this).val() == 'PLAY') {
+      $(this).val('STOP');
+      mySound.load();
+      mySound.play();
+      createChars();  
+    	for(index = 0; index < keys_pressed.length; index++) {  	  
+    	  delay = 1500;
+    	  if(index > 0) {
+    	    delay = keys_pressed[index][0] - keys_pressed[index-1][0];
+    	  }
+    	  global_time += delay;
+    	  (function(index) {
+    	    setTimeout(function(){ simulateKeyPress(keys_pressed[index][1]); }, global_time);
+    	  })(index)
+    	}	
+    } else {
+      mySound.pause();
+      createChars();
+    }
+    
+  });
+  
+  
 });
 
 $(document).keydown(function(e) {
 	
 	var d = new Date();
 	
-	keys_pressed[i] = [d.getTime(), e.which];
-	
+	if($('#record').val() != 'record') {
+	  keys_pressed[i] =  new Array(d.getTime(), e.which);
+  }
+
 	i++;
-			
+					
     switch(e.which) {
         case 37: // left arrow - Char1 left
-        	phil.charCommands(ctx, {'move':["'left'",10]});
-        break;
+        	//alert('left');
+        	phil.charCommands(ctx, {'move':["'left'"]});
+            break;
 
         case 38: // up arrow - Char1 up
-        	phil.charCommands(ctx, {'move':["'up'",10]});
-        break;
+        	// if(!isPixelCollision(phil.img, phil.x + 20, phil.y + 20, mimi.img, mimi.x, mimi.y, false)) {
+        	phil.charCommands(ctx, {'move':["'up'"]});
+        	// }
+        	break;
 
         case 39: // right arrow - Char1 right
-        	phil.charCommands(ctx, {'move':["'right'",10]});
-        break;
+        	// Collision Detection
+        	// if(!isPixelCollision(phil.img, phil.x + 20, phil.y + 20, mimi.img, mimi.x, mimi.y, false)) {
+        	phil.charCommands(ctx, {'move':["'right'"]});	
+        	// }
+        	break;
 
         case 40: // down arrow - Char1 down
-        	phil.charCommands(ctx, {'move':["'down'",10]});
-        break;
+        	phil.charCommands(ctx, {'move':["'down'"]});
+        	break;
         
         case 32: // space key - Char1 talk
         	phil.charCommands(ctx, {'talk':[]});
-        break;
+            break;
         
         case 18: // ALT key - Char1 sit
         	phil.charCommands(ctx, {'sit':[]});
-        break;
+        	break;
         
         case 9: // TAB key - Char2 talk
         	mimi.charCommands(ctx, {'talk':[]});
-        break;
+        	break;
         
         case 87: // w key - Char2 up
-        	mimi.charCommands(ctx, {'move':["'up'",10]});
-        break;
+        	mimi.charCommands(ctx, {'move':["'up'"]});
+        	break;
         
         case 83: // s key - Char2 down
-        	mimi.charCommands(ctx, {'move':["'down'",10]});
-        break;
+        	mimi.charCommands(ctx, {'move':["'down'"]});
+            break;
         
         case 65: // a key - Char2 left
-        	mimi.charCommands(ctx, {'move':["'left'",10]});
-        break;
+        	mimi.charCommands(ctx, {'move':["'left'"]});
+            break;
         
         case 68: // d key - Char2 right
-        	mimi.charCommands(ctx, {'move':["'right'",10]});
-        break;
+        	mimi.charCommands(ctx, {'move':["'right'"]});
+            break;
+        
+        case 20: // Caps Lock key - Char2 sit
+        	mimi.charCommands(ctx, {'sit':[]});
+            break;
 
         default: return; // exit this handler for other keys
     }
     e.preventDefault(); // prevent the default action (scroll / move caret)
     
-    //alert(keys_pressed);
-    
 });
+
+/**
+ * @author Joseph Lenton - PlayMyCode.com
+ *
+ * @param first An ImageData object from the first image we are colliding with.
+ * @param x The x location of 'first'.
+ * @param y The y location of 'first'.
+ * @param other An ImageData object from the second image involved in the collision check.
+ * @param x2 The x location of 'other'.
+ * @param y2 The y location of 'other'.
+ * @param isCentred True if the locations refer to the centre of 'first' and 'other', false to specify the top left corner.
+ */
+function isPixelCollision( first, x, y, other, x2, y2, isCentred )
+{
+    // we need to avoid using floats, as were doing array lookups
+    x  = Math.round( x );
+    y  = Math.round( y );
+    x2 = Math.round( x2 );
+    y2 = Math.round( y2 );
+
+    var w  = first.width,
+        h  = first.height,
+        w2 = other.width,
+        h2 = other.height ;
+
+    // deal with the image being centred
+    if ( isCentred ) {
+        // fast rounding, but positive only
+        x  -= ( w/2 + 0.5) << 0
+        y  -= ( h/2 + 0.5) << 0
+        x2 -= (w2/2 + 0.5) << 0
+        y2 -= (h2/2 + 0.5) << 0
+    }
+
+    // find the top left and bottom right corners of overlapping area
+    var xMin = Math.max( x, x2 ),
+        yMin = Math.max( y, y2 ),
+        xMax = Math.min( x+w, x2+w2 ),
+        yMax = Math.min( y+h, y2+h2 );
+
+    // Sanity collision check, we ensure that the top-left corner is both
+    // above and to the left of the bottom-right corner.
+    if ( xMin >= xMax || yMin >= yMax ) {
+        return false;
+    }
+
+    var xDiff = xMax - xMin,
+        yDiff = yMax - yMin;
+
+    // get the pixels out from the images
+    var pixels  = first.data,
+        pixels2 = other.data;
+
+    // if the area is really small,
+    // then just perform a normal image collision check
+    if ( xDiff < 4 && yDiff < 4 ) {    	
+        for ( var pixelX = xMin; pixelX < xMax; pixelX++ ) {
+            for ( var pixelY = yMin; pixelY < yMax; pixelY++ ) {
+                if (
+                        ( pixels [ ((pixelX-x ) + (pixelY-y )*w )*4 + 3 ] !== 0 ) &&
+                        ( pixels2[ ((pixelX-x2) + (pixelY-y2)*w2)*4 + 3 ] !== 0 )
+                ) {
+                	alert('true');
+                    return 'true';
+                }
+            }
+        }
+    } else {
+        /* What is this doing?
+         * It is iterating over the overlapping area,
+         * across the x then y the,
+         * checking if the pixels are on top of this.
+         *
+         * What is special is that it increments by incX or incY,
+         * allowing it to quickly jump across the image in large increments
+         * rather then slowly going pixel by pixel.
+         *
+         * This makes it more likely to find a colliding pixel early.
+         */
+
+        // Work out the increments,
+        // it's a third, but ensure we don't get a tiny
+        // slither of an area for the last iteration (using fast ceil).
+        var incX = xDiff / 3.0,
+            incY = yDiff / 3.0;
+        incX = (~~incX === incX) ? incX : (incX+1 | 0);
+        incY = (~~incY === incY) ? incY : (incY+1 | 0);
+        
+        for ( var offsetY = 0; offsetY < incY; offsetY++ ) {
+            for ( var offsetX = 0; offsetX < incX; offsetX++ ) {
+                for ( var pixelY = yMin+offsetY; pixelY < yMax; pixelY += incY ) {
+                    for ( var pixelX = xMin+offsetX; pixelX < xMax; pixelX += incX ) {
+                    	return true;
+                    	/*
+                    	if (
+                                ( pixels [ ((pixelX-x ) + (pixelY-y )*w )*4 + 3 ] !== 0 ) &&
+                                ( pixels2[ ((pixelX-x2) + (pixelY-y2)*w2)*4 + 3 ] !== 0 )
+                        ) {
+                        	return 'true';
+                        }
+                        */
+                    }
+                }
+            }
+        }
+        
+    }
+
+    return false;
+}
