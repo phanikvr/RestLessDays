e5dc8d292c5c7e91e8bfd3e0ec047e2c2c07e538
Paco Bahamonde <paco@filmtronic.com>
2016-08-11 22:46:21 +1000

    New Basic Headers - Bug found in /the-restless-days/include/header_basic.php

--- include/header_basic.php
+++ include/header_basic.php
@@ -2,44 +2,31 @@
 /**
  * The Header Basic Include File (a bare necessities header).
  */
+$title = "FT - Film Reviews and Computer Games";
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
-  <title><?php print $app_title;?></title>
+  <title><?php print $title;?></title>
   <meta name="description" content="Film Reviews and Computer Games" />
-  <link rel="stylesheet" type="text/css" href="./css/style.css" />
-  <!--
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-  <script src="./js/core.js?<?=substr( md5(rand()), 0, 7);?>" type="text/javascript" charset="utf-8"></script>
-  -->
+  <link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
   <script src="./js/jquery-2.1.1.min.js"></script>
   <script src="./js/code-do-not-upload.js?<?php print substr( md5(rand()), 0, 7); ?>" type="text/javascript" charset="utf-8"></script>
   <script src="./js/buzz.min.js" type="text/javascript" charset="utf-8"></script>
   <script src="./js/game.js"></script>
 </head>
 <body>
-<!--
-<header style="background-color:#EAEAEA;padding: 4px; height: 50px">
-  <a style="float: left" id="logo" href="/"><img width="50" src="./images/logo_small_green_navy_filmtronic.png" /></a>
-  <div class="app_title">&nbsp;&nbsp;<?php print $app_title;?></div>
---> 
-  <!--
-  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-  <input id="back" type="button" value="< B A C K" />
+<header style="background-color:#EAEAEA;padding: 4px; height: 30px">
+  <a name="top"></a>
+  <a style="float: left" id="logo" href="/"><img width="30" src="./images/logo_small_green_navy_filmtronic.png" /></a>
+  <a id="top_logo_text" href="/"><img border="0" width="150" style="float: left;vertical-align: top; margin-left: 10px;" src="/images/logo_text_navy_green_filmtronic.png" /></a>
+      <div id="tm_header_symbol">&reg;</div>
+  <div class="logo_small_text">&nbsp;&nbsp;Film Reviews and Computer Games</div>
   &nbsp;&nbsp;
-  <input id="instructions" type="button" value="I N S T R U C T I O N S" />
-  &nbsp;&nbsp;
-  -->
-  <!--
-  &nbsp;&nbsp;
-  <input id="zoom" type="button" value="ZOOM" />
-  &nbsp;&nbsp;
-  <input id="wide" type="button" value="WIDE" />
-  &nbsp;&nbsp;
-  <input id="refresh" type="button" value="R E F R E S H" />
-  &nbsp;&nbsp;
-  -->
-  <!-- <input id="record" type="button" value="RECORD" /> -->
-    
+  <div class="top_links">
+    <?php if($_SERVER["PHP_SELF"] == "../about-us.php") { ?><b class="navy">About Us</b><?php } else {?><a href="../about-us.php">About Us</a><?php }?>
+    &nbsp;&nbsp;
+    <?php if($_SERVER["PHP_SELF"] == "../how-it-works.php") { ?><b class="navy">How it Works</b><?php } else {?><a href="../how-it-works.php">How it Works</a><?php }?>
+  </div>
+  <div class="app_title">&nbsp;&nbsp;<?php # print $title;?></div>
 </header>
