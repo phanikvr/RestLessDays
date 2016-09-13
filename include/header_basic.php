89531fd2736a83c03eaeec83d9afd873302c0cd5
Paco Bahamonde <paco@filmtronic.com>
2016-09-13 10:17:18 +1000

    code-do-not-upload.js in Use - Some Comment

--- include/header_basic.php
+++ include/header_basic.php
@@ -13,7 +13,7 @@ $title = "FT - Film Reviews and Computer Games";
   <script src="./js/jquery-2.1.1.min.js"></script>
   <!--
   <script src="./js/core.js?<?php// print substr( md5(rand()), 0, 7); ?>" type="text/javascript" charset="utf-8"></script>
-  -->
+  --> 
   <script src="./js/dialogue.js?<?php print substr( md5(rand()), 0, 7); ?>" type="text/javascript" charset="utf-8"></script>  
   <script src="./js/code-do-not-upload.js?<?php print substr( md5(rand()), 0, 7); ?>" type="text/javascript" charset="utf-8"></script>
   <script src="./js/buzz.min.js" type="text/javascript" charset="utf-8"></script>
